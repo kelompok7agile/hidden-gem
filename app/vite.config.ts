fac79a7e112f108313b366001ccce18755e22a4a
dev_piyo <alvioadjijanuar152@gmail.com>
2025-04-09 21:37:34 +0700

    Update vite-config-js

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import commonjs from "@rollup/plugin-commonjs";
+
 const basenameProd = "";
 
 export default defineConfig(({ command }) => {
@@ -9,15 +9,7 @@ export default defineConfig(({ command }) => {
 
   return {
     base: isProd ? basenameProd : "/",
-    plugins: [
-      react(),
-      commonjs({
-        include: "node_modules/**",
-        transformMixedEsModules: true,
-        ignoreTryCatch: false,
-        requireReturnsDefault: "auto",
-      }),
-    ],
+    plugins: [react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
@@ -27,15 +19,16 @@ export default defineConfig(({ command }) => {
       include: ["react", "react-dom"],
     },
     define: {
-      // global: {
-      //   basename: isProd ? basenameProd : "",
-      // },
       global: "globalThis",
     },
     build: {
       commonjsOptions: {
         transformMixedEsModules: true,
       },
+    },
+    esbuild: {
+      jsx: "automatic",
+      target: "esnext",
     },
   };
 });
