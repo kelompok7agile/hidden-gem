329080a2db07ae46575c807d33462550cd0afe4c
hidayatfajar <fajarnh67@gmail.com>
2025-04-29 08:06:16 +0700

    Update Footer component: replace hardcoded author name with dynamic value from appConfig

--- app/src/components/layouts/Footer.tsx
+++ app/src/components/layouts/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     return (
         <footer className="flex flex-col items-center justify-center gap-4 min-h-[3rem] md:h-20 py-2 md:flex-row">
             <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                Made With ❤️ by Fajar Hidayat
+                Made With ❤️ by {appConfig?.author.name}
             </p>
         </footer>
 
