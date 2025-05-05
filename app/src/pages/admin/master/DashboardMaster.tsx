6cf7a68d7b0951880a127570f6844b6077dc4b41
hidayatfajar <fajarnh67@gmail.com>
2025-05-05 19:22:49 +0700

    Enhance DashboardMaster layout: add height to sidebar and improve item styling

--- app/src/pages/admin/master/DashboardMaster.tsx
+++ app/src/pages/admin/master/DashboardMaster.tsx
@@ -24,17 +24,17 @@ const DashboardMaster = () => {
 
     return (
         <div className='grid grid-cols-12 gap-4'>
-            <div className='col-span-3 rounded-lg bg-white'>
+            <div className='col-span-3 rounded-lg bg-white h-[40vh]'>
                 <p className='bg-primary rounded-lg text-white px-3 py-2 font-semibold mb-4'>Menu</p>
                 {moduleList.map((item) => (
                     <div
                         key={item.value}
                         onClick={() => navigate(item.path)}
                         className={clsx(
-                            "flex flex-col items-start justify-center gap-2 py-3 px-2 w-full cursor-pointer rounded-md transition-all duration-200 ease-in-out",
+                            "flex flex-col items-start justify-center gap-2 py-3 px-2 w-full cursor-pointer rounded-md transition-all duration-200 ease-in-out text-neutral-500",
                             {
-                                "bg-primary/80 text-white border-l-4 border-primary": item.value === jenis,
-                                "hover:bg-gray-100 text-gray-700": item.value !== jenis,
+                                "bg-[#fff7ed] border-l-4 border-[#ff961b]": item.value === jenis,
+                                "hover:bg-gray-100": item.value !== jenis,
                             }
                         )}
                     >
@@ -45,7 +45,6 @@ const DashboardMaster = () => {
                     </div>
                 ))}
             </div>
-
             <div className='col-span-9 rounded-lg ring-1'>
                 {SelectedComponent ? <SelectedComponent /> : <div className='flex items-center justify-center h-full'>404 Not Found</div>}
             </div>
