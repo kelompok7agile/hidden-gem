82cbed90e5d995d940269a117d8b679f2c7609cf
hidayatfajar <fajarnh67@gmail.com>
2025-05-05 20:47:35 +0700

    Refactor kategori management: streamline dialog handling and integrate useMaster for data fetching

--- app/src/pages/admin/master/kategori/index.tsx
+++ app/src/pages/admin/master/kategori/index.tsx
@@ -2,9 +2,7 @@ import React, { useState } from 'react'
 import {
     Table,
     TableBody,
-    TableCaption,
     TableCell,
-    TableFooter,
     TableHead,
     TableHeader,
     TableRow,
@@ -13,11 +11,9 @@ import {
 import {
     Dialog,
     DialogContent,
-    DialogDescription,
     DialogFooter,
     DialogHeader,
     DialogTitle,
-    DialogTrigger,
 } from "@/components/ui/dialog"
 
 import { Button } from '@/components/ui/button'
@@ -25,136 +21,201 @@ import { Icon } from '@iconify/react'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 import IconPicker from '@/components/ui/icon-picker'
+import { useMaster } from '@/hooks/admin/useMaster'
 
-const invoices = [
+
+const kategoriTempat = [
     {
-        invoice: "INV001",
-        paymentStatus: "Paid",
-        totalAmount: "$250.00",
-        paymentMethod: "Credit Card",
+        "kategori_tempat_id": 8,
+        "nama": "Semua",
+        "icon": "material-symbols:select-all"
     },
     {
-        invoice: "INV002",
-        paymentStatus: "Pending",
-        totalAmount: "$150.00",
-        paymentMethod: "PayPal",
+        "kategori_tempat_id": 9,
+        "nama": "Vintage",
+        "icon": "hugeicons:vintage-clock"
     },
     {
-        invoice: "INV003",
-        paymentStatus: "Unpaid",
-        totalAmount: "$350.00",
-        paymentMethod: "Bank Transfer",
+        "kategori_tempat_id": 10,
+        "nama": "Nature",
+        "icon": "material-symbols:nature-outline"
     },
     {
-        invoice: "INV004",
-        paymentStatus: "Paid",
-        totalAmount: "$450.00",
-        paymentMethod: "Credit Card",
+        "kategori_tempat_id": 11,
+        "nama": "Minimalist",
+        "icon": "solar:box-minimalistic-broken"
     },
     {
-        invoice: "INV005",
-        paymentStatus: "Paid",
-        totalAmount: "$550.00",
-        paymentMethod: "PayPal",
+        "kategori_tempat_id": 12,
+        "nama": "Industrial",
+        "icon": "mdi:industrial"
     },
     {
-        invoice: "INV006",
-        paymentStatus: "Pending",
-        totalAmount: "$200.00",
-        paymentMethod: "Bank Transfer",
-    },
-    {
-        invoice: "INV007",
-        paymentStatus: "Unpaid",
-        totalAmount: "$300.00",
-        paymentMethod: "Credit Card",
-    },
+        "kategori_tempat_id": 13,
+        "nama": "Cozy",
+        "icon": "solar:home-outline"
+    }
 ]
 const master = 'kategori'
+
 const index = () => {
+    const {
+        data,
+        isLoading,
+        fetchNextPage,
+        hasNextPage,
+        create,
+        update,
+        remove,
+    } = useMaster('kategori-tempat');
 
+    const kategoriTempat = data?.pages.flatMap(page => page.data) || []
 
-    const [dialog, setDialog] = useState({
-        open: false,
-        jenis: "tambah",
-        id: null,
-    })
-
-    const [form, setForm] = useState({
-        id: null,
-        name: "",
-        icon: "",
-    })
-
-    const handleChange = (value: string) => {
-        console.log('value', value)
-        setForm({ ...form, icon: value })
+    interface DialogState {
+        open: boolean;
+        jenis: string;
+        kategori_tempat_id: number[] | null;
     }
 
+    interface FormState {
+        kategori_tempat_id: number[] | null;
+        name: string;
+        icon: string;
+    }
+
+    const [dialog, setDialog] = useState<DialogState>({
+        open: false,
+        jenis: "tambah",
+        kategori_tempat_id: null,
+    });
+
+    const [form, setForm] = useState<FormState>({
+        kategori_tempat_id: null,
+        name: "",
+        icon: "",
+    });
+
+    const handleChange = (value: string) => {
+        console.log('value', value);
+        setForm({ ...form, icon: value });
+    }
+
+    interface Items {
+        kategori_tempat_id: number;
+        nama: string;
+        icon: string;
+    }
+
+    const showDialog = (jenis: string, data: Items) => {
+        setDialog({ open: true, jenis, kategori_tempat_id: [data.kategori_tempat_id] });
+        setForm({ ...form, kategori_tempat_id: [data.kategori_tempat_id], name: data.nama, icon: data.icon });
+
+        console.log('form', form);
+    }
+
+    const closeDialog = () => {
+        setDialog({ ...dialog, open: false });
+        setForm({ ...form, kategori_tempat_id: null, name: "", icon: "" });
+    }
+
+    const handleSubmit = async () => {
+        console.log('clicked', form);
+        if (dialog.jenis === 'tambah') {
+            await create({ nama: form.name, icon: form.icon });
+        } else if (dialog.jenis === 'ubah') {
+            if (form.kategori_tempat_id && form.kategori_tempat_id[0] !== undefined) {
+                await update({ id: form.kategori_tempat_id[0], payload: { nama: form.name, icon: form.icon } });
+            } else {
+                console.error("kategori_tempat_id is null or undefined");
+            }
+        }
+        closeDialog()
+    }
 
     return (
         <>
-            <div className='text-right'>
-                <Dialog>
-                    <DialogTrigger asChild>
-                        <Button variant="outline" size='default' className='mb-2 bg-[#ff961b] text-white hover:bg-[#ff961b]/90 hover:text-white'>
-                            <Icon icon="ic:baseline-add" width={20} height={20} className='mr-2' />
-                            Tambah
-                        </Button>
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px]">
-                        <DialogHeader>
-                            <DialogTitle className='capitalize'> {dialog.jenis} {master}</DialogTitle>
-                            {/* <DialogDescription>
-                                Make changes to your profile here. Click save when you're done.
-                            </DialogDescription> */}
-                        </DialogHeader>
-                        <div>
-                            <div className='mb-4'>
-                                <Label htmlFor="name" className="text-sm font-medium">Nama</Label>
-                                <Input id="name" placeholder={`Masukkan nama ${master}`} className="mt-1" />
-                            </div>
-                            <div className='mb-4'>
-                                <Label htmlFor="icon" className="text-sm font-medium">Icon</Label>
-                                <IconPicker
-                                    value={form.icon}
-                                    onChange={(value) =>
-                                       handleChange(value)
-                                    }
-                                    showSearch={true}
-                                    limit={60}
-                                />
-                            </div>
+            <Dialog open={dialog.open} onOpenChange={(isOpen) => {
+                if (!isOpen) closeDialog()
+            }}>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle className='capitalize'>
+                            {dialog.jenis} {master}
+                        </DialogTitle>
+                    </DialogHeader>
+                    <div>
+                        <div className='mb-4'>
+                            <Label htmlFor="name">Nama</Label>
+                            <Input
+                                id="name"
+                                placeholder={`Masukkan nama ${master}`}
+                                value={form.name}
+                                onChange={(e) => setForm({ ...form, name: e.target.value })}
+                                autoComplete="off"
+                                autoFocus
+                                disabled={isLoading}
+                            />
                         </div>
-                        <DialogFooter>
-                            <Button type="submit">Save changes</Button>
-                        </DialogFooter>
-                    </DialogContent>
-                </Dialog>
+                        <div className='mb-4'>
+                            <Label htmlFor="icon">Icon</Label>
+                            <IconPicker
+                                value={form.icon}
+                                onChange={handleChange}
+                                showSearch={true}
+                                limit={60}
+                            />
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <div className="flex justify-center items-center gap-2 w-full">
+                            <Button className='px-12 w-full' variant="outline" onClick={closeDialog}>Batal</Button>
+                            <Button className='px-12 w-full' type="submit" onClick={handleSubmit}
+                                disabled={isLoading}>{
+                                    isLoading ? "Loading..." : dialog.jenis === 'tambah' ? "Simpan" : "Ubah"
+                                }</Button>
+                        </div>
+                    </DialogFooter>
 
+                </DialogContent>
+            </Dialog>
+
+            <div className='text-right'>
+                <Button variant="outline" size='default' className='mb-2 bg-[#ff961b] text-white hover:bg-[#ff961b]/90 hover:text-white' onClick={() => showDialog('tambah', { kategori_tempat_id: 0, nama: '', icon: '' })}>
+                    <Icon icon="ic:baseline-add" width={20} height={20} className='mr-2' />
+                    Tambah
+                </Button>
             </div>
             <p className='bg-primary rounded-lg text-white px-3 py-2 font-semibold mb-4 capitalize'>Kategori</p>
             <div className='py-2'>
                 <Table>
                     <TableHeader>
                         <TableRow>
-                            <TableHead className="w-[100px]">Invoice</TableHead>
-                            <TableHead>Status</TableHead>
-                            <TableHead>Method</TableHead>
-                            <TableHead className="text-right">Amount</TableHead>
+                            <TableHead className="w-[50px]">No</TableHead>
+                            <TableHead>Nama</TableHead>
+                            <TableHead>Icon</TableHead>
                         </TableRow>
                     </TableHeader>
                     <TableBody>
-                        {invoices.map((invoice) => (
-                            <TableRow key={invoice.invoice}>
-                                <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                                <TableCell>{invoice.paymentStatus}</TableCell>
-                                <TableCell>{invoice.paymentMethod}</TableCell>
-                                <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                        {kategoriTempat.map((item, index) => (
+                            <TableRow key={item.kategori_tempat_id}>
+                                <TableCell>{index + 1}</TableCell>
+                                <TableCell>{item.nama}</TableCell>
+                                <TableCell>
+                                    <Icon icon={item.icon} width={24} height={24} />
+                                </TableCell>
+                                <TableCell className='flex items-center justify-end gap-2'>
+                                    <Button variant="outline" size="icon" onClick={() => showDialog('ubah', item)}>
+                                        <Icon icon="ic:baseline-edit" width={20} height={20} className='text-[#ff961b]' />
+                                    </Button>
+                                    <Button variant="outline" size="icon">
+                                        <Icon icon="ic:baseline-delete" width={20} height={20} className='text-red-500' />
+                                    </Button>
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
+
             </div >
         </>
     )
