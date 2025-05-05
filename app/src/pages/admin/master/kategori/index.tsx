ff57d623aeff61e44ab54ca8106abdb0e04afe7b
hidayatfajar <fajarnh67@gmail.com>
2025-05-05 19:22:55 +0700

    Add Dialog and IconPicker components: implement dialog for adding categories with icon selection

--- app/src/pages/admin/master/kategori/index.tsx
+++ app/src/pages/admin/master/kategori/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Table,
     TableBody,
@@ -9,6 +9,22 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+import { Button } from '@/components/ui/button'
+import { Icon } from '@iconify/react'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import IconPicker from '@/components/ui/icon-picker'
 
 const invoices = [
     {
@@ -54,12 +70,71 @@ const invoices = [
         paymentMethod: "Credit Card",
     },
 ]
-
+const master = 'kategori'
 const index = () => {
+
+
+    const [dialog, setDialog] = useState({
+        open: false,
+        jenis: "tambah",
+        id: null,
+    })
+
+    const [form, setForm] = useState({
+        id: null,
+        name: "",
+        icon: "",
+    })
+
+    const handleChange = (value: string) => {
+        console.log('value', value)
+        setForm({ ...form, icon: value })
+    }
+
+
     return (
         <>
+            <div className='text-right'>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant="outline" size='default' className='mb-2 bg-[#ff961b] text-white hover:bg-[#ff961b]/90 hover:text-white'>
+                            <Icon icon="ic:baseline-add" width={20} height={20} className='mr-2' />
+                            Tambah
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                            <DialogTitle className='capitalize'> {dialog.jenis} {master}</DialogTitle>
+                            {/* <DialogDescription>
+                                Make changes to your profile here. Click save when you're done.
+                            </DialogDescription> */}
+                        </DialogHeader>
+                        <div>
+                            <div className='mb-4'>
+                                <Label htmlFor="name" className="text-sm font-medium">Nama</Label>
+                                <Input id="name" placeholder={`Masukkan nama ${master}`} className="mt-1" />
+                            </div>
+                            <div className='mb-4'>
+                                <Label htmlFor="icon" className="text-sm font-medium">Icon</Label>
+                                <IconPicker
+                                    value={form.icon}
+                                    onChange={(value) =>
+                                       handleChange(value)
+                                    }
+                                    showSearch={true}
+                                    limit={60}
+                                />
+                            </div>
+                        </div>
+                        <DialogFooter>
+                            <Button type="submit">Save changes</Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
+
+            </div>
             <p className='bg-primary rounded-lg text-white px-3 py-2 font-semibold mb-4 capitalize'>Kategori</p>
-            <div className='px-3 py-2'>
+            <div className='py-2'>
                 <Table>
                     <TableHeader>
                         <TableRow>
@@ -80,7 +155,7 @@ const index = () => {
                         ))}
                     </TableBody>
                 </Table>
-            </div>
+            </div >
         </>
     )
 }
