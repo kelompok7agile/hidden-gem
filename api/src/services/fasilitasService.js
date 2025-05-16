14d42107bb19500eb6bfbca1bccee6204f184883
hidayatfajar <fajarnh67@gmail.com>
2025-05-16 20:56:04 +0700

    Add getAllFasilitas function: implement paginated retrieval of fasilitas data with error handling

--- api/src/services/fasilitasService.js
+++ api/src/services/fasilitasService.js
@@ -2,15 +2,39 @@ const fasilitasRepository = require("../repositories/fasilitasRepository");
 
 const getAll = async () => await fasilitasRepository.getAll();
 
-const tambah = async ({ nama }) => {
+const getAllFasilitas = async ({
+  cari = null,
+  sort = "fasilitas_id.asc",
+  page = 1,
+  limit = 10,
+}) => {
+  try {
+    const data = await fasilitasRepository.getAllWithPagination({
+      cari,
+      sort,
+      page,
+      limit,
+    });
+    if (!data) {
+      throw new Error("Gagal mengambil data kategori tempat");
+    }
+    console.log("data", data);
+    return {
+      data,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+const tambah = async ({ nama, icon }) => {
   const existing = await fasilitasRepository.findByNama(nama);
   if (existing) throw new Error("Nama fasilitas sudah terdaftar");
 
-  return await fasilitasRepository.insert({ nama });
+  return await fasilitasRepository.insert({ nama, icon });
 };
 
 const ubah = async (id, data) => await fasilitasRepository.update(id, data);
 
 const hapus = async (id) => await fasilitasRepository.remove(id);
 
-module.exports = { getAll, tambah, ubah, hapus };
+module.exports = { getAllFasilitas, getAll, tambah, ubah, hapus };
