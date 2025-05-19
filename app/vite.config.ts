ed5e4eb407beaa71c9c74c044d0bbc7335bd4ef2
hidayatfajar <fajarnh67@gmail.com>
2025-05-19 20:15:44 +0700

    fix(vite.config): ensure server watch uses polling for file changes

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig(({ command }) => {
     plugins: [react()],
     server: {
       port: 3000,
+      watch: {
+        usePolling: true,
+      },
     },
     resolve: {
       alias: {
